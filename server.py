0912cc318a0c3c50162a5766979d1cfeb3f15b27
Steve Gardner <steve@graceflow.com>
2024-03-20 16:48:34 +1300

    Change port

--- server.py
+++ server.py
@@ -146,4 +146,4 @@ def root():
   return render_template('index.html')
 
 if __name__ == "__main__":
-  app.run(debug=DEBUG, host='0.0.0.0', port=3033)
+  app.run(debug=DEBUG, host='0.0.0.0', port=8080)
